31235c9dcb4b835a079a20ef7a010e9811b570c6
Henry Leon <henryatusabes@gmail.com>
2022-08-21 21:15:54 +0000

    Add the showTurns funtion to game js and test js

--- scripts/tests/game.test.js
+++ scripts/tests/game.test.js
@@ -7,7 +7,8 @@ const {
     newGame,
     showScore,
     addTurn,
-    lightsOn
+    lightsOn,
+    showTurns
 } = require("../game");
 
 
@@ -36,6 +37,9 @@ describe("game object contains correct keys", () => {
 
     test("choices contain correct ids", () => {
         expect(game.choices).toEqual(["button1", "button2", "button3", "button4"]);
+    });
+    test("turnNumber key exists", () => {
+        expect("turnNumber" in game).toBe(true);
     });
 });
 
@@ -84,6 +88,11 @@ describe("gameplay works correctly", () => {
     test("should add correct class to light up the buttons", () => {
         let button = document.getElementById(game.currentGame[0]);
         lightsOn(game.currentGame[0]);
-        expect(button.classList).toContain(game.currentGame[0] + "light");
+        expect(button.classList).toContain("light");
+    });
+    test("showTurns should update game.turnNumber", () => {
+        game.turnNumber = 42;
+        showTurns();
+        expect(game.turnNumber).toBe(0);
     });
 });
